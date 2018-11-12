b2d76562e236d75f4b0dce03e2a0b63deae974a5
Alist3rCode <lopez.yohann@gmail.com>
2018-11-12 12:19:08 +0100

    first

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        
+        <!-- Required meta tags -->
+        <title>Bienenue sur le site internet de Yohann LOPEZ - Support Informatique et Developpement Web.</title>
+        <meta name="description" content="CV en ligne de Yohann LOPEZ, support informatique, developpement Web, formation et dépannage.">
+	<meta name="author" content="Yohann LOPEZ">
+        
+        <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <!-- Bootstrap CSS -->
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+         
+        <!--Fontawesome CSS-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+
+        <!--CSS Perso-->
+        <link rel="stylesheet" href="css/default.css">
+        <link rel="stylesheet" href="css/layout.css">
+        <link rel="stylesheet" href="css/media-queries.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        
+        <!--Add Jquery-->
+        <script  src="https://code.jquery.com/jquery-3.2.1.js"  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="   crossorigin="anonymous"></script>
+
+
+    </head>
+    <body>
+        
+        <!-- Header
+        ================================================== -->
+        <header id="home">
+
+           <nav id="nav-wrap">
+
+              <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Afficher</a>
+                   <a class="mobile-btn" href="#" title="Hide navigation">Cacher</a>
+
+              <ul id="nav" class="nav">
+                 <li class="current"><a class="smoothscroll" href="#home">Accueil</a></li>
+                 <li><a class="smoothscroll" href="#about">A Propos</a></li>
+                      <li><a class="smoothscroll" href="#resume">Résumé</a></li>
+                 <li><a class="smoothscroll" href="#portfolio">Travaux</a></li>
+                 <li><a class="smoothscroll" href="#testimonials">Témoignages</a></li>
+                 <li><a class="smoothscroll" href="#contact">Contact</a></li>
+              </ul> <!-- end #nav -->
+
+           </nav> <!-- end #nav-wrap -->
+
+           <div class="row banner">
+              <div class="banner-text">
+                 <h1 class="responsive-headline">Je suis Yohann Lopez</h1>
+                 <h3>Je suis un grand <span>fan d'informatique</span> basé à Aix en Provence, <span>technicien support</span> et <span>developpeur web</span> en herbe, j'adore également
+                 travailler le bois, sculpter, créer... Je vous laisse <a class="smoothscroll" href="#about">descendre un peu</a>
+                 et en apprendre un peu plus  <a class="smoothscroll" href="#about">sur moi</a>.</h3>
+                 <hr />
+                 <ul class="social">
+                    <li><a href="https://www.linkedin.com/in/yohann-lopez-19061990/"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="https://www.instagram.com/alistersshop/"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="https://github.com/Alist3rCode"><i class="fab fa-github"></i></a> </li>
+                    
+                 </ul>
+              </div>
+           </div>
+
+           <p class="scrolldown">
+              <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a>
+           </p>
+
+        </header> <!-- Header End -->
+
+        
+        
+        
+        
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCo_lLa3e8UeMBQdc6EYS5Wbw7udYxl3_o"></script>
+        <script src="js/modernizr.js"></script>
+    </body>
+
+</html>
