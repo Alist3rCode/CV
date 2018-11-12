07b6509a631888b54d5519b378ebff39667ce958
Alist3rCode <lopez.yohann@gmail.com>
2018-11-12 14:09:04 +0100

    commit

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <!-- Bootstrap CSS -->
-        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
          
         <!--Fontawesome CSS-->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
@@ -25,6 +25,7 @@
         <link rel="stylesheet" href="css/layout.css">
         <link rel="stylesheet" href="css/media-queries.css">
         <link rel="stylesheet" href="css/magnific-popup.css">
+
         
         <!--Add Jquery-->
         <script  src="https://code.jquery.com/jquery-3.2.1.js"  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="   crossorigin="anonymous"></script>
@@ -35,7 +36,7 @@
         
         <!-- Header
         ================================================== -->
-        <header id="home">
+        <header id="home" style="height: 100vh">
 
            <nav id="nav-wrap">
 
@@ -58,7 +59,7 @@
                  <h1 class="responsive-headline">Je suis Yohann Lopez</h1>
                  <h3>Je suis un grand <span>fan d'informatique</span> basé à Aix en Provence, <span>technicien support</span> et <span>developpeur web</span> en herbe, j'adore également
                  travailler le bois, sculpter, créer... Je vous laisse <a class="smoothscroll" href="#about">descendre un peu</a>
-                 et en apprendre un peu plus  <a class="smoothscroll" href="#about">sur moi</a>.</h3>
+                 et en apprendre plus  <a class="smoothscroll" href="#about">sur moi</a>.</h3>
                  <hr />
                  <ul class="social">
                     <li><a href="https://www.linkedin.com/in/yohann-lopez-19061990/"><i class="fab fa-linkedin-in"></i></a></li>
@@ -74,6 +75,288 @@
            </p>
 
         </header> <!-- Header End -->
+        
+        <!-- about section
+   ================================================== -->
+   <section id="about" style ="height:100vh">  
+
+     	<div class="row about-content">
+
+   		<div class="col-md-6 tab-full">
+
+   			<h3>Profil</h3>
+   			<p>De formation scientifique, puis électronique, je me suis toujours passionné pour les nouvelles technologies et la création. 
+                            <br><b>Pédagogue</b>, je me suis orienté vers le support informatique, tout en continuant d'apprendre divers moyen de créer. 
+                            <br><b>Autodidacte</b>, j'apprend le language web depuis septembre 2017 par moi même.</p>
+
+   			<ul class="info-list">
+   				<li>
+   					<strong>Nom : </strong>
+   					<span>Yohann LOPEZ</span>
+   				</li>
+   				<li>
+   					<strong>Date de Naissance : </strong>
+   					<span>19 Juin 1990</span>
+   				</li>
+   				<li>
+   					<strong>Poste : </strong>
+   					<span>Responsable Support Evolucare Imaging</span>
+   				</li>
+   				<li>
+   					<strong>Site : </strong>
+   					<span>www.yohannlopez.fr</span>
+   				</li>
+   				<li>
+   					<strong>Email : </strong>
+   					<span>contact@yohannlopez.fr</span>
+   				</li>
+
+   			</ul> <!-- /info-list -->
+
+   		</div>
+
+   		<div class="col-md-6 tab-full">
+
+   			<h3>Compétences</h3>
+   			<p>En constant apprentissage, mes compétences ne se limitent pas aux exemples indiqués ci-dessous, mais voici les principales : </p>
+
+				<ul class="skill-bars">
+				   <li>
+				   	<div class="progress">
+                                            <div class="progress-bar bg-dark" role="progressbar" style="width: 90%;">90%</div>
+                                        </div>
+				   	<strong>HTML5 / CSS3</strong>
+				   </li>
+				   <li>
+				   	<div class="progress">
+                                            <div class="progress-bar bg-dark" role="progressbar" style="width: 60%;">60%</div>
+                                        </div>
+				   	<strong>JQuery</strong>
+				   </li>
+				   <li>
+				   	<div class="progress">
+                                            <div class="progress-bar bg-dark" role="progressbar" style="width: 50%;">50%</div>
+                                        </div>
+				   	<strong>PHP</strong>
+				   </li>
+				   <li>
+
+				   	<div class="progress">
+                                            <div class="progress-bar bg-dark" role="progressbar" style="width: 80%;">80%</div>
+                                        </div>
+				   	<strong>SQL</strong>
+				   </li>
+                                   <li>
+
+				   	<div class="progress">
+                                            <div class="progress-bar bg-dark" role="progressbar" style="width: 80%;">80%</div>
+                                        </div>
+				   	<strong>Graphisme</strong>
+				   </li>
+                                   <li>
+
+				   	<div class="progress">
+                                            <div class="progress-bar bg-dark" role="progressbar" style="width: 90%;">90%</div>
+                                        </div>
+				   	<strong>Anglais</strong>
+				   </li>
+                                   <li>
+
+				   	<div class="progress">
+                                            <div class="progress-bar bg-dark" role="progressbar" style="width: 95%;">95%</div>
+                                        </div>
+				   	<strong>Windows</strong>
+				   </li>
+                                   <li>
+
+				   	<div class="progress">
+                                            <div class="progress-bar bg-dark" role="progressbar" style="width: 40%;">40%</div>
+                                        </div>
+				   	<strong>Linux</strong>
+				   </li>
+                                   <li>
+
+				   	<div class="progress">
+                                            <div class="progress-bar bg-dark" role="progressbar" style="width: 20%;">20%</div>
+                                        </div>
+				   	<strong>Réseau</strong>
+				   </li>
+      
+				</ul> <!-- /skill-bars -->		
+
+   		</div>
+
+   	</div>
+
+   	<div class="row button-section">
+   		<div class="col-12">
+   			<a href="#contact" title="contact" class="button stroke smoothscroll">Contactez moi</a>
+   			<a href="#" title="Download CV" class="button button-primary">Télécharger mon CV</a>
+   		</div>   		
+   	</div>
+
+   </section> <!-- /process-->    
+   <!-- resume Section
+   ================================================== -->
+	<section id="resume" class="grey-section">
+
+                    <div class="row section-intro">
+                    <div class="col-twelve">
+
+                            <h5>Resume</h5>
+                            <h1>More of my credentials.</h1>
+
+                            <p class="lead">Lorem ipsum Do commodo in proident enim in dolor cupidatat adipisicing dolore officia nisi aliqua incididunt Ut veniam lorem ipsum Consectetur ut in in eu do.</p>
+
+                    </div>   		
+            </div> <!-- /section-intro--> 
+
+            <div class="row resume-timeline">
+
+                    <div class="col-twelve resume-header">
+
+                            <h2>Work Experience</h2>
+
+                    </div> <!-- /resume-header -->
+
+                    <div class="col-twelve">
+
+                            <div class="timeline-wrap">
+
+                                    <div class="timeline-block">
+
+                                            <div class="timeline-ico">
+                                                    <i class="fa fa-graduation-cap"></i>
+                                            </div>
+
+                                            <div class="timeline-header">
+                                                    <h3>UI Designer</h3>
+                                                    <p>July 2015 - Present</p>
+                                            </div>
+
+                                            <div class="timeline-content">
+                                                    <h4>Awesome Studio</h4>
+                                                    <p>Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi.</p>
+                                            </div>
+
+                                    </div> <!-- /timeline-block -->
+
+                                    <div class="timeline-block">
+
+                                            <div class="timeline-ico">
+                                                    <i class="fa fa-graduation-cap"></i>
+                                            </div>
+
+                                            <div class="timeline-header">
+                                                    <h3>Front-end Developer</h3>
+                                                    <p>July 2014 - June 2015</p>
+                                            </div>
+
+                                            <div class="timeline-content">
+                                                    <h4>Super Cool Agency</h4>
+                                                    <p>Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi incididunt.</p>
+                                            </div>
+
+                                    </div> <!-- /timeline-block -->
+
+                                    <div class="timeline-block">
+
+                                            <div class="timeline-ico">
+                                                    <i class="fa fa-graduation-cap"></i>
+                                            </div>
+
+                                            <div class="timeline-header">
+                                                    <h3>Web Designer</h3>
+                                                    <p>May 2013 - June 2014</p>
+                                            </div>
+
+                                            <div class="timeline-content">
+                                                    <h4>Great Designs Studio</h4>
+                                                    <p>Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi incididunt.</p>
+                                            </div>
+
+                                    </div> <!-- /timeline-block -->
+
+                            </div> <!-- /timeline-wrap -->   			
+
+                    </div> <!-- /col-twelve -->
+
+            </div> <!-- /resume-timeline -->
+
+            <div class="row resume-timeline">
+
+                    <div class="col-twelve resume-header">
+
+                            <h2>Education</h2>
+
+                    </div> <!-- /resume-header -->
+
+                    <div class="col-twelve">
+
+                            <div class="timeline-wrap">
+
+                                    <div class="timeline-block">
+
+                                            <div class="timeline-ico">
+                                                    <i class="fa fa-briefcase"></i>
+                                            </div>
+
+                                            <div class="timeline-header">
+                                                    <h3>Master Degree</h3>
+                                                    <p>July 2015 - Present</p>
+                                            </div>
+
+                                            <div class="timeline-content">
+                                                    <h4>University of Life</h4>
+                                                    <p>Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi.</p>
+                                            </div>
+
+                                    </div> <!-- /timeline-block -->
+
+                                    <div class="timeline-block">
+
+                                            <div class="timeline-ico">
+                                                    <i class="fa fa-briefcase"></i>
+                                            </div>
+
+                                            <div class="timeline-header">
+                                                    <h3>Bachelor Degree</h3>
+                                                    <p>July 2014 - June 2015</p>
+                                            </div>
+
+                                            <div class="timeline-content">
+                                                    <h4>State Design University</h4>
+                                                    <p>Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi incididunt.</p>
+                                            </div>
+
+                                    </div> <!-- /timeline-block -->
+
+                                    <div class="timeline-block">
+
+                                            <div class="timeline-ico">
+                                                    <i class="fa fa-briefcase"></i>
+                                            </div>
+
+                                            <div class="timeline-header">
+                                                    <h3>Bachelor Degree</h3>
+                                                    <p>May 2013 - June 2014</p>
+                                            </div>
+
+                                            <div class="timeline-content">
+                                                    <h4>Design College</h4>
+                                                    <p>Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna consectetur nisi cupidatat laboris esse eiusmod deserunt aute do quis velit esse sed Ut proident cupidatat nulla esse cillum laborum occaecat nostrud sit dolor incididunt amet est occaecat nisi incididunt.</p>
+                                            </div>
+
+                                    </div> <!-- /timeline-block -->
+
+                            </div> <!-- /timeline-wrap -->   			
+
+                    </div> <!-- /col-twelve -->
+
+            </div> <!-- /resume-timeline -->
+
+        </section> <!-- /features -->
+
 
         
         
